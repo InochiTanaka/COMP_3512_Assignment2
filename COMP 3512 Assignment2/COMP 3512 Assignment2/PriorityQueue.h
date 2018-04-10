bf25336b796593296e0c45ee27e94ed2e44dfe2f
ccyang1991 <ccyang1991@hotmail.com>
2018-04-10 14:51:54 -0700

    - Added Patient Class - Added Priority Queue Class/CPP

---
+++ COMP 3512 Assignment2/COMP 3512 Assignment2/PriorityQueue.h
@@ -0,0 +1,57 @@
+#pragma once
+#ifndef __ASN2_PRIORITYQUE_H__
+#define __ASN2_PRIORITYQUE_H__
+
+//====================================================================================================
+//	Priority Queue CLass
+//
+//	A Class that Mimics a Priority Que, but with assignment specific Optimization
+//----------------------------------------------------------------------------------------------------
+//	Inochi Tanaka		Joseph Yang
+//						A00950913
+//====================================================================================================
+
+//----------------------------------------------------------------------------------------------------
+//-- Dependency --------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+
+#include <string.h>
+#include <deque>
+
+// foward decleration
+class Patient;	
+
+//----------------------------------------------------------------------------------------------------
+//-- Class Declerations ------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+
+class PriorityQueue
+{
+public:
+	PriorityQueue();
+	~PriorityQueue();
+
+public:
+	// Mutators 
+	void AddPatient(Patient data);
+	void RemovePatient(Patient data);
+	Patient UpdatePatient(Patient data);
+	// Accessors 
+	Patient GetPatient(std::string name);
+
+	// Local Functions
+	bool Save();
+	bool Load(Patient data);
+	void Paint(Patient data);
+	void Exit();
+
+private:
+	void Update();
+
+
+private:
+	std::deque< std::deque<Patient> > mPriority; // 2D Deque
+
+};
+
+#endif
